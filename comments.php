c43310cbb6b2000ce2ef5bd48165a596668bc515
roldub <3rdubz@gmail.com>
2026-01-28 17:27:53 -0700

    Add comments.php

---
+++ comments.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+// This is a PHP comment
+# This is also a PHP comment
+echo "Hello World!";
+
+/* 
+	This is a multi-line
+	PHP comment
+*/
+?>
+
+</body>
+</html>
