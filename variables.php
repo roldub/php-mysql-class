7277a75b940f8b36e211618f412f314ed0144f7c
roldub <3rdubz@gmail.com>
2026-02-02 11:46:02 -0700

    Add variables.php

---
+++ variables.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+// Info:
+$firstname = "Roland";
+$lastname = "Dreher";
+$address = "P. Sherman, 42 Wallaby Way";
+$phonenum = "928 - 555 - 1234";
+
+// Print info:
+print "<p>My info:<br>$firstname $lastname<br>$address<br>$phonenum</p>";
+
+?>
+</body>
+</html>
