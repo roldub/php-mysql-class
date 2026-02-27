6cf0ec0fe778b89da8236c9a0913f201609dae1c
roldub <3rdubz@gmail.com>
2026-02-27 14:32:56 -0700

    Add books2.php

---
+++ books2.php
@@ -0,0 +1,20 @@
+<?php // Script 8.5 - books2.php
+/* This page lists J.D. Salinger's bibliograhpy. */
+
+// Set page title and include the header file:
+define('TITLE', 'Books by J.D. Salinger');
+include('templates/header2.html');
+
+// Leave the PHP section to displaly HTML
+?>
+
+<h2>J.D. Salinger's Books</h2>
+<ul>
+	<li>The Catcher in the Rye</li>
+	<li>Nine Stories</li>
+	<li>Franny and Zooey</li>
+	<li>Raise the High Roof Beam, Carpenters adn Seymour: An Introduction</li>
+</ul>
+
+<?php include('templates/footer.html');
+?>
