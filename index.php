a951325b2e1e40b5a0d69b18521aa11ebeef098f
roldub <3rdubz@gmail.com>
2026-02-27 14:00:20 -0700

    Add index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php // Script 8.4 - index.php
+/* This is the home page for this site. It uses templates to create the layout. */
+
+// Include the header:
+include('templates/header.html');
+// Leave the PHP section to dispaly lots of HTML:
+?>
+
+<h2>Welcome to a J.D. Salinger Fan Club</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla turpis sit amet
+iaculis sagittis. Nullam pellentesque commodo lacus ut hendrerit. Nulla bibendum rutrum dui,
+a suscipit tortor bibendum id. Nunc venenatis maximus lectus, non cursus sapien elementum in.
+Fusce eleifend malesuada turpis ut elementum. Sed condimentum, velit sit amet egestas semper,
+nibh urna sodales arcu, eu vestibulum risus nisi vitae odio. Duis molestie urna in justo
+suscipit lacinia. Praesent maximus urna lobortis felis pellentesque, quis viverra enim
+convallis. Pellentesque eget hendrerit ante. Maecenas rutrum, metus id efficitur gravida,
+velit libero vulputate orci, at aliquet tortor nulla in massa.</p>
+
+<?php // Return to PHP.
+include('templates/footer.html');
+// Include the footer.
+?>
